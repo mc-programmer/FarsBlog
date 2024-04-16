15de0f63b7ac46715156512e784cb56f7c0c2a22
alipour <00senario@gmail.com>
2024-04-16 23:24:02 +0330

    Controllers Started

--- FarsBlog.Web/Areas/Admin/Controllers/ArticleCategoryController.cs
+++ FarsBlog.Web/Areas/Admin/Controllers/ArticleCategoryController.cs
@@ -1,12 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FarsBlog.Application.Services.Interfaces.Article;
+using FarsBlog.Domain.DTOs.ViewModels.Article.Category;
+using Microsoft.AspNetCore.Mvc;
 
-namespace FarsBlog.Web.Areas.Admin.Controllers
+namespace FarsBlog.Web.Areas.Admin.Controllers;
+
+public class ArticleCategoryController : AdminBaseController
 {
-    public class ArticleCategoryController : AdminBaseController
+    #region Fields
+
+    private readonly IArticleCategoryService _articleCategoryService;
+
+    #endregion
+
+    #region Constructor
+
+    public ArticleCategoryController(IArticleCategoryService articleCategoryService)
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+        _articleCategoryService = articleCategoryService;
     }
+
+    #endregion
+
+    #region Actions
+
+    public async Task<IActionResult> List(FilterArticleCategoryViewModel filter)
+    {
+        filter.TakeEntity = 10;
+
+        var result = await _articleCategoryService.FilterArticleCategoriesAsync(filter);
+
+        return View(result);
+    }
+
+    #endregion
 }
