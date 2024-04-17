56551b9ad9d77ebd298248437faa56becd06f5d8
alipour <00senario@gmail.com>
2024-04-17 23:47:21 +0330

    Add Serilog To Project

---
+++ FarsBlog.Web/Configurations/LogsConfig.cs
@@ -0,0 +1,17 @@
+﻿using Serilog;
+
+namespace FarsBlog.Web.Configurations;
+
+public static class LogsConfiger
+{
+    public static void ConfigeSerilog(IHostBuilder hostBuilder)
+    {
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .WriteTo.Console()
+            .WriteTo.File("logs/serilog-logs.txt", rollingInterval: RollingInterval.Day)
+            .CreateLogger();
+
+        hostBuilder.UseSerilog();
+    }
+}
